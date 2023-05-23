c51d94dda35443f070dd9056a8ced341f7d5f5a6
Jiyeon Ko <goprawgo@gmail.com>
2023-05-23 13:43:04 +0900

    ML-8 fix: 라우팅이 제대로 되지 않는 문제 해결

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/home/Home';
 import MagazineDetail from './pages/magazineDetail/MagazineDetail';
 import Map from './pages/map/Map';
 import MyPage from './pages/myPage/MyPage';
-import ProductList from './pages/productDetail/ProductDetail';
+import ProductList from './pages/productList/ProductList';
 import ProductDetail from './pages/productDetail/ProductDetail';
 import Splash from './pages/splash/Splash';
 
@@ -11,13 +11,13 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={Splash} />
-        <Route path="/home" element={Home} />
-        <Route path="/magazine/:id" element={MagazineDetail} />
-        <Route path="/map" element={Map} />
-        <Route path="/productList" element={ProductList} />
-        <Route path="/product/:id" element={ProductDetail} />
-        <Route path="/myPage" element={MyPage} />
+        <Route path="/" element={<Splash />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/magazine/:id" element={<MagazineDetail />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/productList" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/myPage" element={<MyPage />} />
       </Routes>
     </div>
   );
