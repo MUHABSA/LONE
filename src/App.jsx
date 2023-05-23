8b8c6cdaadd987de6784d59b923af107625c2d35
Jiyeon Ko <goprawgo@gmail.com>
2023-05-23 11:29:43 +0900

    ML-8 feat: 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import MagazineDetail from './pages/magazineDetail/MagazineDetail';
+import Map from './pages/map/Map';
+import MyPage from './pages/myPage/MyPage';
+import ProductList from './pages/productDetail/ProductDetail';
+import ProductDetail from './pages/productDetail/ProductDetail';
+import Splash from './pages/splash/Splash';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={Splash} />
+        <Route path="/home" element={Home} />
+        <Route path="/magazine/:id" element={MagazineDetail} />
+        <Route path="/map" element={Map} />
+        <Route path="/productList" element={ProductList} />
+        <Route path="/product/:id" element={ProductDetail} />
+        <Route path="/myPage" element={MyPage} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
