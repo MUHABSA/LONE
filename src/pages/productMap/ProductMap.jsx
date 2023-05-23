a0e02fd16017ee45c5d1812bd228ab1200213760
Jiyeon Ko <goprawgo@gmail.com>
2023-05-23 21:16:14 +0900

    ML-15 feat: 카카오맵 API 작업

--- src/pages/productMap/ProductMap.jsx
+++ src/pages/productMap/ProductMap.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProductMap.css';
 import Header from '../../components/common/header/Header';
 import NavBar from '../../components/common/navBar/NavBar';
 import KakaoMap from '../../components/kakaoMap/KakaoMap';
-// import PlaceInfoModal from '../../components/placeInfoModal/PlaceInfoModal';
+import PlaceInfoModal from '../../components/placeInfoModal/PlaceInfoModal';
 
 export default function ProductMap() {
+  const [isMarkerClicked, setIsMarkerClicked] = useState(false);
+
   return (
     <>
       <div>
         <Header />
-        <KakaoMap />
+        <KakaoMap
+          isMarkerClicked={isMarkerClicked}
+          setIsMarkerClicked={setIsMarkerClicked}
+        />
         <NavBar />
       </div>
-      {/* <PlaceInfoModal /> */}
+      {isMarkerClicked ? (
+        <PlaceInfoModal
+          isMarkerClicked={isMarkerClicked}
+          setIsMarkerClicked={setIsMarkerClicked}
+        />
+      ) : null}
     </>
   );
 }
