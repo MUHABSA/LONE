151f9c825dc43ec8e81f28d4a8c1c89db690200d
Jiyeon Ko <goprawgo@gmail.com>
2023-05-25 10:38:35 +0900

    ML-24 design: 모아보기 페이지 헤더 컴포넌트 타이틀 수정

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useMatch } from 'react-router';
 import SearchBar from '../../searchBar/SearchBar';
-import CategoryNav from '../../catetoryNav/CategoryNav';
 
 export default function Header() {
   const matchMap = useMatch('/map');
@@ -12,7 +11,7 @@ export default function Header() {
   return (
     <header>
       {matchMap ? <SearchBar /> : null}
-      {matchProductList ? <CategoryNav /> : null}
+      {matchProductList ? <h3>전통주 모아보기</h3> : null}
       {matchProductDetail ? <button>뒤로가기</button> : null}
       {matchMyPage ? <h3>전통주 스크랩북</h3> : null}
     </header>
