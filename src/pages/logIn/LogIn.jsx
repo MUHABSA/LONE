974bfb02730da9425866ef3ec1263db4cf387392
Jiyeon Ko <goprawgo@gmail.com>
2023-05-28 22:34:53 +0900

    ML-26 feat: firebase 로그인,로그아웃 기능 구현

---
+++ src/pages/logIn/LogIn.jsx
@@ -0,0 +1,86 @@
+import React, { useContext, useEffect, useState } from 'react';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { IsLoginContext } from '../../context/IsLoginContext';
+import { useNavigate } from 'react-router-dom';
+
+export default function LogIn() {
+  const auth = getAuth();
+  const { isLogin, setIsLogin } = useContext(IsLoginContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (e) {
+      alert('아이디 혹은 비밀번호를 확인하세요');
+      console.log(e);
+    }
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      navigate('/home');
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLogin(true);
+        localStorage.setItem('isLogined', !!user);
+      } else {
+        setIsLogin(false);
+        localStorage.setItem('isLogined', !!user);
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">
+          이메일
+          <input
+            type="text"
+            onChange={handleChangeEmail}
+            value={email}
+            placeholder="이메일을 입력해주세요"
+          />
+        </label>
+        <br />
+        <label htmlFor="">
+          비밀번호
+          <input
+            type="password"
+            onChange={handleChangePassword}
+            value={password}
+            placeholder="비밀번호를 입력해주세요"
+            autoComplete="off"
+          />
+        </label>
+        <button type="button" onClick={login}>
+          로그인
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/pages/splash/Splash.jsx
+++ src/pages/splash/Splash.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import logo from '../../assets/img/logo.png';
 import kakaoLogo from '../../assets/img/kakao-login-logo.png';
 import splashBg from '../../assets/img/bg-splash.png';
+import { useNavigate } from 'react-router-dom';
 
 const SplashWrap = styled.div`
   background-image: url(${splashBg});
@@ -29,6 +30,11 @@ const LoginBtn = styled.button`
   padding: 5px 0;
   border-radius: 25px;
   border: none;
+  &:last-child {
+    //임시 스타일링
+    margin-top: 10px;
+    background-color: #ddc9fd70;
+  }
 `;
 
 const KakaoLogo = styled.img`
@@ -46,16 +52,19 @@ const LoginTxt = styled.h1`
 `;
 
 export default function Splash() {
+  const navigate = useNavigate();
   return (
     <SplashWrap>
       <Logo src={logo} />
       <SplashText>전통주로 로컬을 잇다, 로네</SplashText>
-
       <LoginBtn>
         <LoginTxt>
           <KakaoLogo src={kakaoLogo} />
           카카오로 시작하기
         </LoginTxt>
+      </LoginBtn>
+      <LoginBtn onClick={() => navigate('/login')}>
+        <LoginTxt>이메일 로그인</LoginTxt>
       </LoginBtn>
     </SplashWrap>
   );
