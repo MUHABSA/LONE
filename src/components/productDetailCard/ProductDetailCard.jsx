6ad78743d9a5060d74d9657ff032e657144a2397
Jiyeon Ko <goprawgo@gmail.com>
2023-05-29 17:35:02 +0900

    ML-30 feat: 주류 상세페이지 좋아요 토글 기능 구현

--- src/components/productDetailCard/ProductDetailCard.jsx
+++ src/components/productDetailCard/ProductDetailCard.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function ProductDetailCard({ productDetail }) {
   return (
-    <div>
-      <img src="" alt="" />
-      <strong>{productDetail.product_name}</strong>
-      <p>{productDetail.seller}</p>
-      <p>
-        도수 <span>{productDetail.abv}</span>/ml
-      </p>
-      <button>
-        마셔봤어요 아이콘<span>{productDetail.likeCount}</span>
-      </button>
-    </div>
+    <Link to={`/product/${productDetail.product_id}`}>
+      <div>
+        <img src={productDetail.image} alt="" />
+        <strong>{productDetail.product_name}</strong>
+        <p>{productDetail.seller}</p>
+        <p>
+          도수 <span>{productDetail.abv}</span>/ml
+        </p>
+        <p>
+          <span>{productDetail.likeCount}</span>명이 연결했어요
+        </p>
+        {/* 모아보기 페이지 좋아요 기능도 넣을 경우 아래 이미지는 추후 버튼으로 변경 */}
+        <img src="" alt="로네 링크 이미지" />
+      </div>
+    </Link>
   );
 }
