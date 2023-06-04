1cb61c22e4783e6be59e36623fbb63fc730a69e1
Jiyeon Ko <goprawgo@gmail.com>
2023-06-04 17:04:40 +0900

    ML-40 feat: 랭킹탭 주류 클릭시 상세페이지로 이동하도록 수정

--- src/components/RankItemInfo.jsx
+++ src/components/RankItemInfo.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../Firebase';
 import LikeBtn from './common/LikeBtn';
+import { useNavigate } from 'react-router-dom';
 
 export default function RankItemInfo({ data, rank }) {
+  const navigate = useNavigate();
+  const buttonRef = useRef();
   const [productId, setProductId] = useState(data.product_id);
   const [isLiked, setIsLiked] = useState(data.liked);
   const [likeCount, setLikeCount] = useState(data.likeCount);
@@ -26,8 +29,14 @@ export default function RankItemInfo({ data, rank }) {
     getCurrentProductData();
   }, []);
 
+  const handleClick = (e) => {
+    if (e.target !== buttonRef.current) {
+      navigate(`/product/${data.product_id}`);
+    }
+  };
+
   return (
-    <div>
+    <div onClick={handleClick}>
       <p>{rank + 1}</p>
       <img src={data.image} alt="" />
       <strong>{data.product_name}</strong>
@@ -36,6 +45,7 @@ export default function RankItemInfo({ data, rank }) {
         <span>{likeCount}</span>명이 연결됐어요
       </p>
       <LikeBtn
+        ref={buttonRef}
         productId={productId}
         isLiked={isLiked}
         setIsLiked={setIsLiked}
