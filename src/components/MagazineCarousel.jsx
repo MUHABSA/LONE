0a9e3563fb47858498f9f010fadaa289f2167c12
Jiyeon Ko <goprawgo@gmail.com>
2023-06-22 14:52:25 +0900

    ML-52 design: 캐러셀 arrow 버튼 추가

--- src/components/MagazineCarousel.jsx
+++ src/components/MagazineCarousel.jsx
@@ -48,7 +48,7 @@ const setting = {
   infinite: true, //무한 반복 옵션
   draggable: true, //드래그 가능 여부
   dots: true, // 스크롤바 아래 점으로 페이지네이션 여부
-  arrows: false, // 옆으로 이동하는 화살표 버튼 표시 여부
+  arrows: true, // 옆으로 이동하는 화살표 버튼 표시 여부
   speed: 700, // dot, arrow 눌렀을 때 다음 슬라이드로 완전히 넘어가는데까지 걸리는 시간(ms)
   fade: false, // 페이드아웃 형식으로 다음 컨텐츠 보여주고 싶을 때(드래그해도 페이드아웃으로 넘어감)
   fadeSpeed: 700, // 페이드아웃 시간 (ms)
@@ -97,6 +97,7 @@ const SlickSlider = styled(Slider)`
   .slick-dots {
     position: absolute;
     bottom: 45px; //스크롤 컨트롤러 위아래로 조절하려면 이거 조정하면 됩니다
+    z-index: 2;
     li {
       padding: 0 50px; // button::before의 width값의 반으로 주면 dot끼리 딱 붙음(참고)
       opacity: 0.5;
@@ -116,5 +117,40 @@ const SlickSlider = styled(Slider)`
         background-color: #be98fd;
       }
     }
+  }
+
+  // arrow 스타일링
+  .slick-prev,
+  .slick-next {
+    width: 100px; //호버했을 시 진하게 표시되는 부분 너비
+    height: 500px;
+    z-index: 1; //ProductRank z-index가 2라서 1로 설정했습니다(랭킹탭 뒤로 숨겨지도록)
+  }
+  .slick-prev {
+    left: 0;
+  }
+  .slick-next {
+    right: 0;
+  }
+  .slick-prev:before,
+  .slick-next:before {
+    display: block;
+    height: 500px; //호버했을 시 진하게 표시되는 부분 높이(캐러셀 이미지 사이즈에 맞춤)
+    font-size: 0px; //1px 이상으로 설정하면 화살표 버튼이 나타납니다
+  }
+  // 그라데이션 설정
+  .slick-prev:hover:before {
+    background: linear-gradient(
+      90deg,
+      rgba(0, 0, 0, 0.30976897594975494) 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+  }
+  .slick-next:hover:before {
+    background: linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 0) 0%,
+      rgba(0, 0, 0, 0.30976897594975494) 100%
+    );
   }
 `;
